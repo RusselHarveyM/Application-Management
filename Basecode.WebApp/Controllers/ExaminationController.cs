de80de69bfef057ea0c29750bc29355e1fd8f7d9
kuanhh <hannahbenitez62@gmail.com>
2023-07-13 00:13:41 +0800

    Setup initial Examination controller

---
+++ Basecode.WebApp/Controllers/ExaminationController.cs
@@ -0,0 +1,22 @@
+﻿using Basecode.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class ExaminationController : Controller
+    {
+        private readonly IExaminationService _service;
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public ExaminationController(IExaminationService service)
+        {
+            _service = service;
+        }
+
+        public void Shortlist()
+        {
+
+        }
+    }
+}
