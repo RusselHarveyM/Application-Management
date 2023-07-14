5255f964112de22c8cc0ef5040efa90bc215d4fc
kuanhh <hannahbenitez62@gmail.com>
2023-07-14 21:16:31 +0800

    Move shortlisting logic to service

--- Basecode.Services/Interfaces/IExaminationService.cs
+++ Basecode.Services/Interfaces/IExaminationService.cs
@@ -4,6 +4,8 @@ namespace Basecode.Services.Interfaces
 {
     public interface IExaminationService
     {
-        List<Examination> GetAllExaminations();
+        List<Examination> GetExaminationsByJobOpeningId(int jobOpeningId);
+        List<Examination> ShortlistExaminations(int jobOpeningId);
+        double GetShortlistPercentage(int totalExams);
     }
 }
