994dfbece1dbeba306905bc6b3784f1087ae190c
kuanhh <hannahbenitez62@gmail.com>
2023-07-14 21:17:07 +0800

    Modify shortlist function in controller

--- Basecode.WebApp/Controllers/ExaminationController.cs
+++ Basecode.WebApp/Controllers/ExaminationController.cs
@@ -1,5 +1,6 @@
 ﻿using Basecode.Data.Models;
 using Basecode.Services.Interfaces;
+using Basecode.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 using NLog;
 
@@ -15,55 +16,19 @@ namespace Basecode.WebApp.Controllers
             _service = service;
         }
 
-        public List<Examination> Shortlist()
+        public IActionResult Shortlist(int jobOpeningId)
         {
-            List<Examination> exams = new List<Examination>()
-            { 
-                new Examination
-                {
-                    Id = 1,
-                    UserId_HR = 1,
-                    Date = new DateOnly(2023,1,1),
-                    TeamsLink = "link.test",
-                    Score = 79,
-                    Result = "PASS",
-                    ApplicationId = 1
-                },
-                new Examination
-                {
-                    Id = 2,
-                    UserId_HR = 2,
-                    Date = new DateOnly(2023,1,1),
-                    TeamsLink = "link.test",
-                    Score = 75,
-                    Result = "PASS",
-                    ApplicationId = 2
-                },
-                new Examination
-                {
-                    Id = 3,
-                    UserId_HR = 3,
-                    Date = new DateOnly(2023,1,1),
-                    TeamsLink = "link.test",
-                    Score = 95,
-                    Result = "PASS",
-                    ApplicationId = 3
-                },
-                new Examination
-                {
-                    Id = 4,
-                    UserId_HR = 4,
-                    Date = new DateOnly(2023,1,1),
-                    TeamsLink = "link.test",
-                    Score = 89,
-                    Result = "PASS",
-                    ApplicationId = 4
-                },
-            };
-
-            exams = exams.OrderByDescending(a => a.Score).ToList();
-            List<Examination> shortlistedExams = exams.Take(2).ToList();
-            return shortlistedExams;
+            try
+            {
+                List<Examination> shortlistedExams = _service.ShortlistExaminations(jobOpeningId);
+                return Ok(shortlistedExams);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(ErrorHandling.DefaultException(e.Message));
+                return StatusCode(500, "Something went wrong.");
+            }
         }
+
     }
 }
