c52b7b675d9980dc62575fb74b8e67ca743b1015
kuanhh <hannahbenitez62@gmail.com>
2023-07-20 02:07:09 +0800

    Create view models for the Assign Users partial view

---
+++ Basecode.Data/ViewModels/ApplicantStatusViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Basecode.Data.ViewModels
+{
+    public class ApplicantStatusViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Status { get; set; }
+    }
+}
