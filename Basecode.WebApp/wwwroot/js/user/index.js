ed0932f10a09c543783c6867a5f9b0ae9ad7a52c
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-21 01:36:22 +0800

    add code to index.js

--- Basecode.WebApp/wwwroot/js/user/index.js
+++ Basecode.WebApp/wwwroot/js/user/index.js
@@ -12,6 +12,13 @@ function showEditModal(id) {
     });
 }
 
+function showAssignModal(id) {
+    $.get("/User/AssignView/" + id, function (data) {
+        $("#assignModalContainer").html(data);
+        $("#assignUserModal").modal("show");
+    });
+}
+
 function showDeleteModal(id) {
     $.get("/User/DeleteView/" + id, function (data) {
         $("#deleteModalContainer").html(data);
