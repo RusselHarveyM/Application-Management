e1658ebfb7c2c4c20aa71fd1abc96d68a562fce0
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-07-21 12:00:21 +0800

    add documentation for interfaces

--- Basecode.Services/Interfaces/IEmailService.cs
+++ Basecode.Services/Interfaces/IEmailService.cs
@@ -7,14 +7,44 @@ using System.Threading.Tasks;
 
 namespace Basecode.Services.Interfaces
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IEmailService
     {
+        /// <summary>
+        /// Sends the notify email.
+        /// </summary>
+        /// <param name="applicant">The applicant.</param>
+        /// <param name="newStatus">The new status.</param>
+        /// <returns></returns>
         Task SendNotifyEmail(Applicant applicant, string newStatus);
 
+        /// <summary>
+        /// Sends the status notification.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="applicant">The applicant.</param>
+        /// <param name="newStatus">The new status.</param>
+        /// <returns></returns>
         Task SendStatusNotification(User user, Applicant applicant, string newStatus);
 
+        /// <summary>
+        /// Sends the approval email.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="applicant">The applicant.</param>
+        /// <param name="appId">The application identifier.</param>
+        /// <param name="newStatus">The new status.</param>
+        /// <returns></returns>
         Task SendApprovalEmail(User user, Applicant applicant, Guid appId, string newStatus);
 
+        /// <summary>
+        /// Sends the rejected email.
+        /// </summary>
+        /// <param name="applicant">The applicant.</param>
+        /// <param name="newStatus">The new status.</param>
+        /// <returns></returns>
         Task SendRejectedEmail(Applicant applicant, string newStatus);
 
         Task SendRegretEmail(Applicant applicant, string job);
