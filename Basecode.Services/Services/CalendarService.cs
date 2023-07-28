8c73aa076a5ce082b87dff09784cf25662df6c30
kuanhh <hannahbenitez62@gmail.com>
2023-07-28 19:12:02 +0800

    Pass User Principal Name instead of ObjectId

--- Basecode.Services/Services/CalendarService.cs
+++ Basecode.Services/Services/CalendarService.cs
@@ -2,7 +2,6 @@
 using Basecode.Services.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Graph.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
@@ -22,12 +21,12 @@ public class CalendarService : ICalendarService
         tokensFile = _environment.ContentRootPath + @"\tokens.json";
     }
 
-    public string CreateEvent(CalendarEvent calendarEvent)
+    public string CreateEvent(CalendarEvent calendarEvent, string email)
     {
         var tokens = JObject.Parse(File.ReadAllText(tokensFile));
-        var user = _config["GraphApi:ObjectId"];
+        //var user = _config["GraphApi:ObjectId"];
         
-        var restClient = new RestClient($"https://graph.microsoft.com/v1.0/users/{user}/calendar/events");
+        var restClient = new RestClient($"https://graph.microsoft.com/v1.0/users/{email}/calendar/events");
         var restRequest = new RestRequest();
 
         restRequest.AddHeader("Authorization", "Bearer " + tokens["access_token"].ToString());
