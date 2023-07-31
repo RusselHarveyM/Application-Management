2f89bfe111ec74c5899efae7f0d0761160e7b622
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-31 13:15:56 +0800

    update basecode and add CurrenHireRepository

--- Basecode.Data/Interfaces/ICurrentHireRepository.cs
+++ Basecode.Data/Interfaces/ICurrentHireRepository.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Basecode.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Basecode.Data.Models;
 
 namespace Basecode.Data.Interfaces
 {
---
+++ Basecode.Data/Repositories/CurrentHireRepository.cs
@@ -0,0 +1,31 @@
+﻿using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Repositories
+{
+    public class CurrentHireRepository : BaseRepository, ICurrentHireRepository
+    {
+        private readonly BasecodeContext _context;
+
+        public CurrentHireRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get user offer by Id
+        /// </summary>
+        /// <param name="userOfferId"></param>
+        /// <returns></returns>
+        public CurrentHire GetUserOfferById(int userOfferId)
+        {
+            return _context.CurrentHire.Find(userOfferId);
+        }
+    }
+}
