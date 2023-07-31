e3d7122cdf22cdf2d95c1aacaf02bb677c7ad1f4
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-07-31 13:08:29 +0800

    update current hire model

--- Basecode.Data/Models/CurrentHire.cs
+++ Basecode.Data/Models/CurrentHire.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Basecode.Data.Models
 {
-    internal class CurrentHire
+    public class CurrentHire
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Guid ApplicationId { get; set; }
+        public string Firstname { get; set; }
+        public string? Middlename { get; set; }
+        public string Lastname { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Status { get; set; } = "hired";
+
+        public User User { get; set; } = null!;
+        public Application Application { get; set; } = null!;
     }
 }
