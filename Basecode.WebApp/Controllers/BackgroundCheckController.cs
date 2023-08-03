17ef23abedaea4c49a9fd0cc015647da38a856a1
Jaymark Carba <jaymark.carba@cit.edu>
2023-08-03 15:46:07 +0800

    Update routing to use tokens for security

--- Basecode.WebApp/Controllers/BackgroundCheckController.cs
+++ Basecode.WebApp/Controllers/BackgroundCheckController.cs
@@ -18,67 +18,73 @@ public class BackgroundCheckController : Controller
     private readonly IJobOpeningService _jobOpeningService;
     private readonly IEmailSendingService _emailSendingService;
     private readonly IUserService _userService;
-    private readonly SignInManager<IdentityUser> _signInManager;
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly TokenHelper _tokenHelper;
 
     public BackgroundCheckController(ICharacterReferenceService characterReferenceService,
         IApplicantService applicantService,
         IApplicationService applicationService, IJobOpeningService jobOpeningService,
         IBackgroundCheckService backgroundCheckService,
-        SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager,
-        IEmailSendingService emailSendingService, IUserService userService)
+        IEmailSendingService emailSendingService, IUserService userService, IConfiguration config)
     {
         _characterReferenceService = characterReferenceService;
         _applicantService = applicantService;
         _applicationService = applicationService;
         _jobOpeningService = jobOpeningService;
         _backgroundCheckService = backgroundCheckService;
-        _signInManager = signInManager;
-        _userManager = userManager;
         _emailSendingService = emailSendingService;
         _userService = userService;
+        _tokenHelper = new TokenHelper(config["TokenHelper:SecretKey"]);
     }
 
-    //[Route("/BackgroundCheck/Form/{characterReferenceId}/{userId}")]
-    public IActionResult Index()
+    [Route("/BackgroundCheck/Form/{token}")]
+    public IActionResult Index(string token)
     {
-        //to be implemented
-        return View();
-    }
-
-    [Route("/BackgroundCheck/Form/{characterReferenceId}/{userId}")]
-    public IActionResult Form(int characterReferenceId, int userId)
-    {
-        _logger.Trace("BackgroundCheck Controller | Enter form successfully");
-
+        _logger.Trace("Enter form successfully");
         try
         {
-            //Check first if referee already answered form
-            var isAnswered = _backgroundCheckService.GetBackgroundByCharacterRefId(characterReferenceId);
-            if (isAnswered != null)
+            //Check token is for request reference related
+            var tokenClaims = _tokenHelper.GetTokenClaims(token, "RequestReference");
+            if (tokenClaims.Count == 0)
             {
-                ViewBag.IsFormSubmitted = false;
-                return View("Redirection");
+                _logger.Warn("Invalid or Expired token");
+                return RedirectToAction("Index", "Home");
             }
+            //Check if value is given
+            if (tokenClaims.TryGetValue("userId", out var userIdRetrieve) && tokenClaims.TryGetValue("characterReferenceId", out var characterReferenceIdRetrieve))
+            {
+                if (int.TryParse(userIdRetrieve, out int userId) && int.TryParse(characterReferenceIdRetrieve, out int characterReferenceId))
+                {
+                    //Check first if referee already answered form
+                    var isAnswered = _backgroundCheckService.GetBackgroundByCharacterRefId(characterReferenceId);
+                    if (isAnswered != null)
+                    {
+                        ViewBag.IsFormSubmitted = false;
+                        return View("Redirection");
+                    }
 
-            //Get applicant id
-            var applicantId = _characterReferenceService.GetApplicantIdByCharacterReferenceId(characterReferenceId);
-            if (applicantId <= 0) return NotFound();
-            //Get applicant
-            var applicantDetails = _applicantService.GetApplicantById(applicantId);
-            //Get application
-            var applicationDetails = _applicationService.GetApplicationByApplicantId(applicantId);
-            //Get job
-            var jobDetails = _jobOpeningService.GetById(applicationDetails!.JobOpeningId);
-            //Get character reference
-            var getReference = _characterReferenceService.GetCharacterReferenceById(characterReferenceId);
-            //Ready the applicant details for view
-            ViewData["bgApplicantLastname"] = applicantDetails.Lastname;
-            ViewData["bgApplicantFirstname"] = applicantDetails.Firstname;
-            ViewData["bgApplicantJob"] = jobDetails.Title;
-            ViewData["bgApplicantDate"] = applicationDetails.ApplicationDate.ToShortDateString();
-            ViewData["bgcrId"] = characterReferenceId;
-            ViewData["bgUserId"] = userId;
+                    //Get applicant id
+                    var applicantId = _characterReferenceService.GetApplicantIdByCharacterReferenceId(characterReferenceId);
+                    if (applicantId <= 0) return NotFound();
+                    //Get applicant
+                    var applicantDetails = _applicantService.GetApplicantById(applicantId);
+                    //Get application
+                    var applicationDetails = _applicationService.GetApplicationByApplicantId(applicantId);
+                    //Get job
+                    var jobDetails = _jobOpeningService.GetById(applicationDetails!.JobOpeningId);
+                    //Ready the applicant details for view
+                    ViewData["bgApplicantLastname"] = applicantDetails.Lastname;
+                    ViewData["bgApplicantFirstname"] = applicantDetails.Firstname;
+                    ViewData["bgApplicantJob"] = jobDetails.Title;
+                    ViewData["bgApplicantDate"] = applicationDetails.ApplicationDate.ToShortDateString();
+                    ViewData["bgcrId"] = characterReferenceId;
+                    ViewData["bgUserId"] = userId;
+                }
+                else
+                {
+                    _logger.Warn("Invalid Id claims");
+                    return RedirectToAction("Index", "Home");
+                }
+            }
         }
         catch (Exception e)
         {
@@ -92,11 +98,18 @@ public class BackgroundCheckController : Controller
     [Route("/BackgroundCheck/FormOk")]
     public async Task<ActionResult> FormOk(BackgroundCheckFormViewModel data)
     {
-        _logger.Trace("Form Submitted Successfully");
-        _backgroundCheckService.Create(data);
-        ViewBag.IsFormSubmitted = true;
+        try
+        {
+            _logger.Trace("Form Submitted Successfully");
+            _backgroundCheckService.Create(data);
+            ViewBag.IsFormSubmitted = true;
 
-
-        return View("Redirection");
+            return View("Redirection");
+        }
+        catch(Exception e)
+        {
+            _logger.Error(ErrorHandling.DefaultException(e.Message));
+            return StatusCode(500, "Something went wrong.");
+        }
     }
 }
