572b60694ea5a76ec10b75857d6a0d89fba7534e
kuanhh <hannahbenitez62@gmail.com>
2023-08-04 00:16:44 +0800

    Add view model

---
+++ Basecode.Data/ViewModels/ConfirmedViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Basecode.Data.ViewModels
+{
+    public class ConfirmedViewModel
+    {
+        public List<ConfirmedApplicantViewModel> ConfirmedApplicants { get; set; }
+        public List<ConfirmedApplicantViewModel> OnboardingApplicants { get; set; }
+    }
+}
